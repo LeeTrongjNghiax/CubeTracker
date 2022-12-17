42f839bec0a65268fd3a2b0df3ccb2313d3045a9
LeeTrongjNghiax <leetrongjnghiax0938225745@gmail.com>
2022-12-17 13:28:51 +0700

    Make button functionable

---
+++ JS/eventListener.js
@@ -0,0 +1,7 @@
+changeState = (e) => {
+  e.setAttribute(
+    "data-state", 
+    e.getAttribute("data-state") == "Not Learned" ? "Learning" : 
+      e.getAttribute("data-state") == "Learning" ? "Learned" : "Not Learned"
+  );
+}
