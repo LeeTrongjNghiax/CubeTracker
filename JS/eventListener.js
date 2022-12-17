79d4229731256afe73bf58569b07745c85554a52
LeeTrongjNghiax <leetrongjnghiax0938225745@gmail.com>
2022-12-17 16:34:25 +0700

    Organize CSS file

--- JS/eventListener.js
+++ JS/eventListener.js
@@ -1,7 +1,6 @@
-changeState = (e) => {
+changeState = (e) => 
   e.setAttribute(
     "data-state", 
     e.getAttribute("data-state") == "Not Learned" ? "Learning" : 
       e.getAttribute("data-state") == "Learning" ? "Learned" : "Not Learned"
   );
-}
